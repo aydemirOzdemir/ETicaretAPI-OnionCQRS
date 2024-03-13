5ff01d4cd1e15e287de013e6bd534a344600ad44
ozdemiraydemir96 <ozdemiraydemir96@gmail.com>
2024-03-13 17:24:12 +0300

    Entitylerin konfigurasyon ayarları yapıldı.

---
+++ Infrastucture/Persistance/E-Ticaret.Persistance/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using E_Ticaret.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Ticaret.Persistance.Configuration;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        Category category1 = new()
+        {
+            Id = 1,
+            Name = "Elektrik",
+            ParentId = 0,
+            Priorty = 1,
+            IsDeleted=false,
+            CreatedDate=DateTime.Now
+        };
+        Category category2 = new()
+        {
+            Id = 2,
+            Name = "Moda",
+            ParentId = 0,
+            Priorty = 2,
+            IsDeleted = false,
+            CreatedDate = DateTime.Now
+        };
+        Category child1 = new()
+        {
+            Id = 3,
+            Name = "Bilgisayar",
+            ParentId = 1,
+            Priorty = 1,
+            IsDeleted = false,
+            CreatedDate = DateTime.Now
+        };
+        Category child2 = new()
+        {
+            Id = 4,
+            Name = "Kadın",
+            ParentId = 2,
+            Priorty = 1,
+            IsDeleted = false,
+            CreatedDate = DateTime.Now
+        };
+        builder.HasData(category1, category2,child1,child2);
+    }
+}
---
+++ Infrastucture/Persistance/E-Ticaret.Persistance/Configuration/DetailConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Bogus;
+using E_Ticaret.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Ticaret.Persistance.Configuration;
+
+public class DetailConfiguration : IEntityTypeConfiguration<Detail>
+{
+    public void Configure(EntityTypeBuilder<Detail> builder)
+    {
+        Faker faker = new Faker("tr");
+        Detail d1 = new Detail()
+        {
+            Id = 1,
+            Title = faker.Lorem.Sentence(1),
+            Description = faker.Lorem.Sentence(5),
+            CategoryId = 1,
+            CreatedDate = DateTime.Now,
+            IsDeleted = false
+        };
+        Detail d2 = new Detail()
+        {
+            Id = 2,
+            Title = faker.Lorem.Sentence(2),
+            Description = faker.Lorem.Sentence(5),
+            CategoryId = 3,
+            CreatedDate = DateTime.Now,
+            IsDeleted = true
+        };
+        Detail d3 = new Detail()
+        {
+            Id = 3,
+            Title = faker.Lorem.Sentence(1),
+            Description = faker.Lorem.Sentence(5),
+            CategoryId = 4,
+            CreatedDate = DateTime.Now,
+            IsDeleted = false
+        };
+        builder.HasData(d1, d2, d3);
+    }
+}
