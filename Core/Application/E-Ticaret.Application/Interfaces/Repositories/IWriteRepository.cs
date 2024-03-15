c26dfbd0564d2c2ebe641f10ac17e69a715f8241
ozdemiraydemir96 <ozdemiraydemir96@gmail.com>
2024-03-15 14:11:40 +0300

    WriteRepository Yapıldı

--- Core/Application/E-Ticaret.Application/Interfaces/Repositories/IWriteRepository.cs
+++ Core/Application/E-Ticaret.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -9,4 +9,9 @@ namespace E_Ticaret.Application.Interfaces.Repositories;
 
 public interface IWriteRepository<T> where T : class,IEntityBase,new()
 {
+    Task AddAsync(T entity);
+    Task AddRangeAsync(IList<T> entities);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(T entity);
+
 }
--- Infrastucture/Persistance/E-Ticaret.Persistance/Repositories/WriteRepository.cs
+++ Infrastucture/Persistance/E-Ticaret.Persistance/Repositories/WriteRepository.cs
@@ -1,5 +1,7 @@
 ﻿using E_Ticaret.Application.Interfaces.Repositories;
 using E_Ticaret.Domain.Common;
+using E_Ticaret.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +10,37 @@ using System.Threading.Tasks;
 
 namespace E_Ticaret.Persistance.Repositories;
 
-public class WriteRepository<T>:IWriteRepository<T> where T : class,IEntityBase,new()
+public class WriteRepository<T> : IWriteRepository<T> where T : class, IEntityBase, new()
 {
+    private readonly ETicaretDbContext context;
+    private DbSet<T> Table { get => context.Set<T>(); }
 
+    public WriteRepository(ETicaretDbContext context)
+    {
+        this.context = context;
+
+    }
+    public async Task AddAsync(T entity)
+    {
+        await Table.AddAsync(entity);
+    }
+
+    public async Task AddRangeAsync(IList<T> entities)
+    {
+        await Table.AddRangeAsync(entities);
+    }
+
+    public async Task<T> DeleteAsync(T entity)
+    {
+        await Task.Run(() => Table.Remove(entity));
+        return entity;
+    }
+
+
+
+    public async Task<T> UpdateAsync(T entity)
+    {
+        await Task.Run(()=>Table.Update(entity));
+        return entity;
+    }
 }
