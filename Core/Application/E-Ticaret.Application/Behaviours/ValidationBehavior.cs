e97994f999b68a05f4d3fc4a557c75ec7769fcf1
ozdemiraydemir96 <ozdemiraydemir96@gmail.com>
2024-03-18 23:20:52 +0300

    Validation işlemleri için örnek bir validation sınıfı yazıldı ve pipeline behaviour oluşturuldu

---
+++ Core/Application/E-Ticaret.Application/Behaviours/ValidationBehavior.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Ticaret.Application.Behaviours;
+
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>  // Mediator da yazmış olduğum yapıda ırequestlerim tip olarak içinde dönen responselarlada işaretlenirler
+{
+    private readonly IEnumerable<IValidator<TRequest>> validator;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validator)
+    {
+        this.validator = validator;
+    }
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var context = new ValidationContext<TRequest>(request);
+        var failtures=validator.Select(v=>v.Validate(context))//buradaki yapıda gelen requesttin içine giriyoruz
+            .SelectMany(v=>v.Errors)//Buradaki tüm errorleri getireceğim grup bayla sadece bir tanesini seçiyorum daha sonra null olmayanları alıp listeliyorum.
+            .GroupBy(e=>e.ErrorMessage)
+            .Select(x=>x.First())
+            .Where(x=>x != null)
+            .ToList();
+        if (failtures.Any())
+            throw new ValidationException(failtures);
+        return next();
+        
+    }
+}
--- Core/Application/E-Ticaret.Application/Registration.cs
+++ Core/Application/E-Ticaret.Application/Registration.cs
@@ -1,7 +1,11 @@
-﻿using E_Ticaret.Application.Exceptions;
+﻿using E_Ticaret.Application.Behaviours;
+using E_Ticaret.Application.Exceptions;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -15,6 +19,9 @@ public static class Registration
     {
         services.AddMediatR(cfg=> cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
         services.AddTransient<ExceptionMiddleware>();
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("tr");
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
         return services;
     }
 }
